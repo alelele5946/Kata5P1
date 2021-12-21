1359ce7bf47fe2fb86dc020e7255f5273414f611
Entrar <Entrar@PC-1191>
2021-12-21 11:14:46 +0000

    KATA5P1: versión 1

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -27,7 +27,8 @@ public class Kata5P1{
 
     public static void main(String[] args) {
             SelectApp app = new SelectApp();
-            createNewTable();
+            //createNewTable();
+            app.SelectAll();
     }
     
 }
